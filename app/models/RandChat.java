8f9c745270a69d4eaef958833d159ec4330084bb
Jiaqi Chen <chen2007999@126.com>
2015-07-09 20:00:56 +0100

    implementing chatting eature with websocket

--- app/models/RandChat.java
+++ app/models/RandChat.java
@@ -12,5 +12,26 @@ public class RandChat {
 
     public static void start(WebSocket.In<String> in, WebSocket.Out<String> out) {
 
+        connections.add(out);
+
+        in.onMessage(new Callback<String>(){
+            public void invoke(String event){
+                RandChat.notifyAll(event);
+            }
+        });
+
+        in.onClose(new Callback0(){
+            public void invoke(){
+                RandChat.notifyAll("A connection closed");
+            }
+        });
+
+    }
+
+    // Iterate connection list and write incoming message
+    public static void notifyAll(String message){
+        for (WebSocket.Out<String> out : connections) {
+            out.write(message);
+        }
     }
 }
