c58672776d2d0e34b6f8544583d15d837518e27f
Jiaqi Chen <chen2007999@126.com>
2015-07-14 08:02:44 +0100

    still fixing randChat & chatPair

--- app/models/RandChat.java
+++ app/models/RandChat.java
@@ -16,13 +16,15 @@ public class RandChat {
         waiting.add(out);
 
         if(waiting.size() >= 2) {
+            waiting.remove(out);
+
             Random rand = new Random();
             int index1 = rand.nextInt(waiting.size());
-            int index2 = rand.nextInt(waiting.size());
-            while(index1 == index2) {
-                index2 = rand.nextInt(waiting.size());
-            }
-            ChatPair chatPair = new ChatPair(waiting.get(index1), waiting.get(index2));
+            WebSocket.Out<String> client1 = waiting.get(index1);
+
+            waiting.remove(index1);
+
+            ChatPair chatPair = new ChatPair(out, client1);
             chatPair.start(in);
 
         }
