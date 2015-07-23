cafd9a22406333ef6355a724b47fa7c43bead896
Jiaqi Chen <chen2007999@126.com>
2015-07-23 20:10:42 +0100

    create popUp for firend profile

--- target/scala-2.11/twirl/main/views/html/friendProfile.template.scala
+++ target/scala-2.11/twirl/main/views/html/friendProfile.template.scala
@@ -30,9 +30,12 @@ Seq[Any](format.raw/*1.25*/("""
 
 """),_display_(/*3.2*/main(client.name)/*3.19*/ {_display_(Seq[Any](format.raw/*3.21*/("""
 
+"""),format.raw/*5.1*/("""<h3>"""),_display_(/*5.6*/client/*5.12*/.name),format.raw/*5.17*/("""</h3>
+<h3>"""),_display_(/*6.6*/client/*6.12*/.email),format.raw/*6.18*/("""</h3>
+"""),_display_(/*7.2*/client/*7.8*/.image),format.raw/*7.14*/("""
 
 
-"""),format.raw/*7.1*/("""yeahhh
+"""),format.raw/*10.1*/("""yeahhh
 """)))}))}
   }
 
@@ -45,11 +48,11 @@ Seq[Any](format.raw/*1.25*/("""
 }
               /*
                   -- GENERATED --
-                  DATE: Wed Jul 22 22:03:29 BST 2015
+                  DATE: Thu Jul 23 20:01:36 BST 2015
                   SOURCE: /Users/jiaqichen/Desktop/RandChat/app/views/friendProfile.scala.html
-                  HASH: 4e8f27c9668889d85917a47961fc953ce4a4c0ed
-                  MATRIX: 738->1|849->24|877->27|902->44|941->46|971->50
-                  LINES: 26->1|29->1|31->3|31->3|31->3|35->7
+                  HASH: eace560b00ace227f4f5c58d24fbed001c019e18
+                  MATRIX: 738->1|849->24|877->27|902->44|941->46|969->48|999->53|1013->59|1038->64|1074->75|1088->81|1114->87|1146->94|1159->100|1185->106|1215->109
+                  LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|33->5|34->6|34->6|34->6|35->7|35->7|35->7|38->10
                   -- GENERATED --
               */
           
--- target/scala-2.11/twirl/main/views/html/index.template.scala
+++ target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -41,15 +41,24 @@ Seq[Any](format.raw/*1.95*/("""
 """)))}),format.raw/*13.2*/("""
 
 
-    """),format.raw/*16.5*/("""<h1>Simple chat</h1>
+    """),format.raw/*16.5*/("""<h1>Welcome to RandChat</h1>
     <h3>current user: """),_display_(/*17.24*/Application/*17.35*/.currentClient.getName()),format.raw/*17.59*/("""</h3>
 
+<script language="javascript">
+var popupWindow = null;
+function centeredPopup(url,winName,w,h,scroll)"""),format.raw/*21.47*/("""{"""),format.raw/*21.48*/("""
+"""),format.raw/*22.1*/("""LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+popupWindow = window.open(url,winName,settings)
+"""),format.raw/*27.1*/("""}"""),format.raw/*27.2*/("""
+"""),format.raw/*28.1*/("""</script>
+<p><button href="friendProfile" onclick="centeredPopup(this.href,'myWindow','700','300','yes');return false">the other user's profile</button></p>
 
-"""),_display_(/*20.2*/helper/*20.8*/.form(routes.Application.friendProfile())/*20.49*/ {_display_(Seq[Any](format.raw/*20.51*/("""
-"""),format.raw/*21.1*/("""<button type="submit">The other user's profile </button>
-""")))}),format.raw/*22.2*/("""
 
-    """),format.raw/*24.5*/("""<div id="the-other-side"></div>
+
+    <div id="the-other-side"></div>
     <div id="socket-messages"></div>
 
     <input type="text" id="socket-input" placeholder="message to send" />
@@ -57,11 +66,10 @@ Seq[Any](format.raw/*1.95*/("""
 <button onclick="nextUser()">Next User</button>
 
 
+    <script type="text/javascript" charset="utf-8" src=""""),_display_(/*41.58*/routes/*41.64*/.Application.wsJs()),format.raw/*41.83*/(""""></script>
 
-    <script type="text/javascript" charset="utf-8" src=""""),_display_(/*33.58*/routes/*33.64*/.Application.wsJs()),format.raw/*33.83*/(""""></script>
 
-
-""")))}),format.raw/*36.2*/("""
+""")))}),format.raw/*44.2*/("""
 
 """))}
   }
@@ -75,11 +83,11 @@ Seq[Any](format.raw/*1.95*/("""
 }
               /*
                   -- GENERATED --
-                  DATE: Wed Jul 22 22:03:08 BST 2015
+                  DATE: Thu Jul 23 20:09:36 BST 2015
                   SOURCE: /Users/jiaqichen/Desktop/RandChat/app/views/index.scala.html
-                  HASH: d1c34531f058390233ff206039bfa6f8ac4b7e8e
-                  MATRIX: 776->1|957->94|985->97|1009->113|1048->115|1078->120|1110->137|1149->139|1178->142|1223->161|1232->162|1258->168|1294->175|1323->178|1358->197|1398->199|1426->200|1473->220|1483->221|1510->227|1547->234|1581->241|1652->285|1672->296|1717->320|1752->329|1766->335|1816->376|1856->378|1884->379|1972->437|2005->443|2285->696|2300->702|2340->721|2385->736
-                  LINES: 26->1|29->1|31->3|31->3|31->3|35->7|35->7|35->7|36->8|36->8|36->8|36->8|37->9|39->11|39->11|39->11|40->12|40->12|40->12|40->12|41->13|44->16|45->17|45->17|45->17|48->20|48->20|48->20|48->20|49->21|50->22|52->24|61->33|61->33|61->33|64->36
+                  HASH: ca2ac3acb666307472850e04d09c1ffd3438c9a8
+                  MATRIX: 776->1|957->94|985->97|1009->113|1048->115|1078->120|1110->137|1149->139|1178->142|1223->161|1232->162|1258->168|1294->175|1323->178|1358->197|1398->199|1426->200|1473->220|1483->221|1510->227|1547->234|1581->241|1660->293|1680->304|1725->328|1861->436|1890->437|1918->438|2218->711|2246->712|2274->713|2717->1129|2732->1135|2772->1154|2817->1169
+                  LINES: 26->1|29->1|31->3|31->3|31->3|35->7|35->7|35->7|36->8|36->8|36->8|36->8|37->9|39->11|39->11|39->11|40->12|40->12|40->12|40->12|41->13|44->16|45->17|45->17|45->17|49->21|49->21|50->22|55->27|55->27|56->28|69->41|69->41|69->41|72->44
                   -- GENERATED --
               */
           
